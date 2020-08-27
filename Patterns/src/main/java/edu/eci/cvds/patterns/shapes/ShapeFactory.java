13d7dc29177911ffbd49cbc7d55f5cfddba663e5
PaulaSanchez <46799459+PaulaSanchez810@users.noreply.github.com>
2020-08-27 12:35:38 -0500

    .gitinore

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -9,13 +9,13 @@ public class ShapeFactory {
     public static Shape create(RegularShapeType type) {
         switch (type) {
             case Hexagon:
-                return new Triangle();
+                return new Hexagon();
             case Pentagon:
                 return new Pentagon();
             case Quadrilateral:
                 return new Quadrilateral();
             case Triangle:
-                return new Hexagon();
+                return new Triangle();
             default:
                 return  null;
 
